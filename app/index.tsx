7f75fb7b9131e20dc3b6bdc1fda1c0abd6685b50
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-01-07 02:14:17 +0000

    Change "Hello World" to "Learn With Tânia"

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Hello World!</Text>
+      <Text className="text-3xl font-pblack">Learn With Tânia</Text>
       <StatusBar style="auto" />
       <Link href="/calendar" style={{ color: 'blue' }}>Go to Home</Link>
     </View>
