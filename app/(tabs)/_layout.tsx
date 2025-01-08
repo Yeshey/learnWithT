dba52faf6502f9e49f2e95fc8c907bc4d570fdd6
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-01-08 00:59:21 +0000

    bottom bar, but it has issues

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,80 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { Tabs , Redirect } from 'expo-router';
 
-/*
-const TabIcon = ({ icon, color, name, focused }) => {
-  return (
-    <View>
-      <Image 
-        source ={}
-      />
-    </View>
-  )
+import { icons } from "../../constants";
 
-*/
+type TabIconProps = {
+  icon: any; // Replace `any` with the correct type if you know the icon's type (e.g., ImageSourcePropType)
+  color: string;
+  name: string;
+  focused: boolean;
+};
+
+const TabIcon: React.FC<TabIconProps> = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
     <>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
+        }}
+      >
         <Tabs.Screen 
           name="home"
-          //options={{
-          //  title: 'Home',
-          //  headerShown: false,
-            //tabBarIcon: ({color, focused }) => (
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-            //)
-          //}}
+        <Tabs.Screen 
+          name="calendar"
+          options={{
+            title: "Calendar",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.calendar}
+                color={color}
+                name="Calendar"
+                focused={focused}
+              />
+            ),
+          }}
         />
 
       </Tabs>
