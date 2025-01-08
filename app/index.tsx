0ea7fdee175df013e5d8a87f03678b74120fe25a
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-01-08 01:50:09 +0000

    onboarding screen, but its wanky

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Text, View, Image } from 'react-native';
+import { Link, router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import { images } from "../constants";
+import CustomButton from '@/components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Learn With Tânia</Text>
-      <StatusBar style="auto" />
-      <Link href="/calendar" style={{ color: 'blue' }}>Go to Home</Link>
-    </View>
+
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView contentContainerStyle={{ height: "100%" }}>
+          <View className="w-full justify-center items-center h-full px-4">
+
+
+            <Image
+              source={images.logo}
+              className="w-[130px] h-[84px]"
+              resizeMode="contain"
+            />
+
+            <Image
+              source={images.cards}
+              className="max-w-[380px] w-full h-[298px]"
+              resizeMode="contain"
+            />
+
+            <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            For your Portuguese Journey
+          </Text>
+
+            <CustomButton
+              title="Continue with Email"
+              handlePress={() => router.push("/sign-in")}
+              containerStyles="w-full mt-7"
+            />
+
+            <Text className="text-3xl font-pblack text-white">Learn With Tânia</Text>
+            <StatusBar style="auto" />
+            <Link href="/calendar" style={{ color: 'green' }}>Go to Home</Link>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
