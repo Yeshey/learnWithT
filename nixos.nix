5f199f66550f79644cf14df2822f20ba50091191
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-01-10 22:44:06 +0000

    messing with nix

--- nixos.nix
+++ nixos.nix
@@ -8,26 +8,43 @@ let
 in
 {
   options.learnWithT = {
-    enable = lib.mkEnableOption "Enable the Learn With T service.";
+    #enable = lib.mkEnableOption "Can disable all options from the module";
 
     development.openPorts = {
       enable = lib.mkOption {
         type = lib.types.bool;
         default = false;
-        description = "Whether to open specific ports for the service.";
+        description = "Whether to open specific ports for `npx expo start`, react native development";
       };
       portsToOpen = lib.mkOption {
         type = lib.types.listOf lib.types.port;
         default = [ 8081 ];
-        description = "List of ports to open for the service. By default opens port 8081";
+        description = "List of ports to open for `npx expo start`, react native development. By default opens port 8081";
       };
     };
+
+    appwrite = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "appwrite local-host docker";
+      };
+    };
+
   };
 
-  config = lib.mkIf cfg.enable {
-    networking.firewall = lib.mkIf cfg.development.openPorts.enable {
-      allowedUDPPorts = cfg.development.openPorts.portsToOpen;
-      allowedTCPPorts = cfg.development.openPorts.portsToOpen;
-    };
-  };
+  config = lib.mkMerge [ ( lib.mkIf ( cfg.development.openPorts.enable /*&& cfg.enable*/) {
+      networking.firewall = {
+        allowedUDPPorts = cfg.development.openPorts.portsToOpen;
+        allowedTCPPorts = cfg.development.openPorts.portsToOpen;
+      };
+    })
+    
+    ( lib.mkIf ( cfg.appwrite.enable /*&& cfg.enable*/) {
+      
+
+
+    })
+  ];
+
 }
