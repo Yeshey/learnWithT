80392eac104cfe4339ea326316b985768e11e62b
Yeshey <yesheysangpo@hotmail.com>
2025-08-14 00:04:54 +0100

    site map for SEO

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc' // Using swc is great!
+import react from '@vitejs/plugin-react-swc'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/learnWithT/', // This is for GitHub Pages
+  plugins: [
+    react(),
+    sitemap({
+      // The hostname MUST include your repository path for GitHub Pages
+      hostname: 'https://Yeshey.github.io/learnWithT',
+      // Provide the list of all your pages here
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/faq',
+        '/translations',
+        '/offer/group-funchal',
+        '/offer/group-online',
+        '/offer/private-1on1',
+        '/offer/private-1on2',
+        '/terms-conditions',
+      ],
+    }),
+  ],
+  base: '/learnWithT/',
   build: {
-    outDir: 'dist' // Ensure output is to 'dist'
+    outDir: 'dist'
   }
 })
